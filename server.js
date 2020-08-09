93393b5a476401a723e541ce3de90299e0b5945d
SondosMatahen <Sondosmatahen@gmail.com>
2020-08-09 17:32:16 +0300

    lab11 step 3&4

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 require('dotenv').config();
 const express=require('express');
 const cors= require('cors');
+const superagent =require('superagent');
 const app = express();
 
 app.set('view engine','ejs');
@@ -15,20 +16,88 @@ app.use(express.static('./public'))
 const PORT =process.env.PORT||3000;
 
 
+
+//------------routes---------
+
 app.get('/', (req,res)=>{
-    res.render('./pages/index');
+    res.render('pages/index');
   });
 
   
 app.get('/hello', (req,res)=>{
-    res.render('./pages/index');
+    res.render('pages/index');
   });
 
   app.get('/search/new', (req,res)=>{
-    res.render('./pages/searches/new');
+    res.render('pages/searches/new');
   });
 
 
+
+
+app.post('/searches',hadnelSearch)
+
+
+
+function hadnelSearch(req,res){
+  let input=req.body.input;
+  let searchBy = req.body.search;
+  let terms;
+
+ if (searchBy=='title'){
+   terms='intitle'
+ }
+ else{
+  terms='inauthor'
+ }
+  
+
+let url=`https://www.googleapis.com/books/v1/volumes?q=${input}+${terms}:${input}`
+ console.log(url)
+  
+  // console.log('qs :', input);
+  // console.log('s :', searchBy);
+  // console.log('POST request: ', req.body);
+  // console.log(req.body);
+
+
+ superagent.get(url).then(data=>{
+let arr=data.body.items.map(e=>{
+  let newbook =new Book(e);
+  return newbook;
+})
+.catch(error=>{
+  console.log(error);
+  res.render('pages/error');
+})
+
+// console.log(arr)
+res.render('pages/searches/show',{list:arr})
+})
+
+}
+
+
+
+
+
+let image= `https://i.imgur.com/J5LVHEL.jpg`
+let reg=/http/gi;
+
+//-----constructor function-----
+function Book(data){
+  this.img=data.volumeInfo.imageLinks.thumbnail ? data.volumeInfo.imageLinks.thumbnail.replace(reg, 'https'):image;
+  this.title=data.volumeInfo.title;
+  this.authors=data.volumeInfo.authors;
+  this.description=data.volumeInfo.description?data.volumeInfo.description:'No Description';
+
+}
+
+
+
+//-----listening to port------
 app.listen(PORT,()=>{
-    console.log(`listening to port ${PORT}`);
+  console.log(`listening to port ${PORT}`);
 });
+
+
